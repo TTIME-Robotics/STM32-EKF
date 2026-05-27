42511ab966dacb2a53687ff1b6723af99e29961c
Daniel Dew <daniel.dew007@gmail.com>
2026-05-27 21:13:53 +0100

    some types
    Update types.h

--- H7-EKF/CM7/TTIME-EKF/Inc/types.h
+++ H7-EKF/CM7/TTIME-EKF/Inc/types.h
@@ -8,5 +8,30 @@
 #ifndef EKF_TYPES_H
 #define EKF_TYPES_H
 
+namespace EKF {
+
+/**
+ * @brief A type containing information about a robot's pose
+ */
+typedef struct {
+	float position_x;     // X position in world frame (m)
+	float position_y;      // Y position in world frame (m)
+	float velocity_u;    // Longitudinal velocity in body frame (pointing out nose) (m/s)
+	float velocity_v;    // Lateral velocity in body frame (pointing out right side) (m/s)
+	float angle;         // The angle from the +ve x-axis (radians)
+	float angular_vel; // Angular frequency/velocity (radians/s)
+} State_t;
+/**
+ * @brief A type containing position and heading.
+ *
+ * More intuitive than State_t
+ */
+typedef struct {
+	float position_x;
+	float position_y;
+	float heading;
+} Pose_t;
+
+}
 
 #endif EKF_TYPES_H
